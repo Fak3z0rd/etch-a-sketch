fc12ac029959e655754a31c90eff30e68926c361
Fak3z0rd <vinicius.meireles@outlook.com>
2022-05-09 12:08:40 -0300

    Add major functions
    1- add dynamical grid
    2- add color picker
    3- add clear grid button
    4- add mode buttons

--- index.js
+++ index.js
@@ -1,17 +1,80 @@
+const $size = document.querySelector("#size");
 const $container = document.getElementById("main");
+const $color = document.querySelector("#color");
+const $gridSize = document.querySelector("#grid-size");
 
-let divQuantity = 16;
+const $clear = document.getElementById("clear");
+const $randomBtn = document.getElementById("random");
+const $normalBtn = document.getElementById("normal");
+const $lightshadeBtn = document.getElementById("lightshade");
+const $darkshadeBtn = document.getElementById("darkshade");
 
-function changeColor(wrapper) {
-    wrapper.classList.add("active");
+let draw = false;
+let drawMode = "normal";
+
+$container.addEventListener("mousedown", () => (draw = true));
+$container.addEventListener("mouseup", () => (draw = false));
+
+$size.addEventListener("change", () => {
+    if ($size.value > 100) {
+        $size.value = 100;
+        alert("Max value is 100");
+    }
+    makeGrid($size.value);
+});
+
+$clear.addEventListener("click", () => {
+    clearCells();
+});
+
+$normalBtn.addEventListener("click", (e) => {
+    setMode(e);
+});
+
+$randomBtn.addEventListener("click", (e) => {
+    setMode(e);
+});
+
+$lightshadeBtn.addEventListener("click", (e) => {
+    setMode(e);
+});
+
+$darkshadeBtn.addEventListener("click", (e) => {
+    setMode(e);
+});
+
+function setMode(e) {
+    drawMode = e.target.id;
 }
 
-for (let i = 0; i < divQuantity * divQuantity; i++) {
-    const div = document.createElement("div");
-    div.className = "square-div";
-    div.textContent = i + 1;
-    div.addEventListener("mouseenter", () => {
-        changeColor(div);
+function changePixelColor(wrapper) {
+    wrapper.style.backgroundColor = $color.value;
+}
+
+function makeGrid(size) {
+    $container.style.setProperty("--size", size);
+    $container.innerHTML = "";
+    $gridSize.textContent = `${size} x ${size}`;
+    for (let i = 0; i < size * size; i++) {
+        const div = document.createElement("div");
+        div.className = "pixel";
+        div.addEventListener("mouseover", () => {
+            if (draw) {
+                changePixelColor(div);
+            }
+        });
+        div.addEventListener("mousedown", () => {
+            changePixelColor(div);
+        });
+        $container.append(div);
+    }
+}
+
+function clearCells() {
+    const cells = document.querySelectorAll(".pixel");
+    cells.forEach((e) => {
+        e.style.backgroundColor = "white";
     });
-    $container.append(div);
 }
+
+makeGrid($size.value);
