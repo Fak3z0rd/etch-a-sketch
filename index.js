5211875f9e4f7a3f73a00e803163f772a83ea038
Fak3z0rd <vinicius.meireles@outlook.com>
2022-05-09 00:42:34 -0300

    add hover effect

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const $container = document.getElementById("main");
 
 let divQuantity = 16;
 
+function changeColor(wrapper) {
+    wrapper.classList.add("active");
+}
+
 for (let i = 0; i < divQuantity * divQuantity; i++) {
     const div = document.createElement("div");
     div.className = "square-div";
-    div.textContent = i+1;
+    div.textContent = i + 1;
+    div.addEventListener("mouseenter", () => {
+        changeColor(div);
+    });
     $container.append(div);
 }
